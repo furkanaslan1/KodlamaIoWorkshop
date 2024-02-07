a2c0d926f36168d413ec92bc454f8ba53952ea8a
gulsum <gulsum@DESKTOP-QHFV6IE>
2024-02-07 15:09:08 +0300

    Add project files.

---
+++ KodlamaIoWorkshop/Entities/Course.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodlamaIoWorkshop.Entities;
+
+public class Course
+
+{
+    public Course()
+    {
+      
+
+    }
+
+    public Course(int ıd, string name, string description, string pictureUrl, bool ısFree, Teacher teacher)
+    {
+        Id = ıd;
+        Name = name;
+        Description = description;
+        PictureUrl = pictureUrl;
+        IsFree = ısFree;
+        Teacher = teacher;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public string PictureUrl { get; set; }
+
+    public bool IsFree { get; set; }
+
+    public Teacher Teacher { get; set; }
+
+    public Category Category{ get; set; }
+}
+
+
+
---
+++ KodlamaIoWorkshop/Entities/Teacher.cs
@@ -0,0 +1,16 @@
+﻿namespace KodlamaIoWorkshop.Entities;
+
+public class Teacher
+
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string ImageURL { get; set; }
+
+    public List<Course> Courses { get; set; }
+
+}
+
